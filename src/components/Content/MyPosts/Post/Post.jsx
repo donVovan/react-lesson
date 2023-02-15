0dbba8c11eec64176532582e8bc101a8bca05693
donVovan <vladimir.a.blinov@gmail.com>
2023-02-15 14:25:16 +0300

    Update 2 files

--- src/components/Content/MyPosts/Post/Post.jsx
+++ src/components/Content/MyPosts/Post/Post.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import s from './Post.module.css';
 
-const Post = () => {
+const Post = (props) => {
     return (
         <div className={s.item}>
             <img
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-ZvKLG2DVdIGFLwLy0U-dsesmG2t361SPMQ&usqp=CAU"
                 alt=""/>
-            post 1 <br/>
+            {props.message} <br/>
             <div>
                 <span>like</span>
             </div>
