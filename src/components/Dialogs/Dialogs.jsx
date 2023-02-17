2d9c5340608fecf9ded0cf67ab0d6c255d3ba432
donVovan <vladimir.a.blinov@gmail.com>
2023-02-17 12:27:37 +0300

    Update 2 files

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,29 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
   return (
-      <div>
-          Dialogs
+      <div className={s.dialogs}>
+          <div className={s.dialogsItems}>
+              <div className={s.dialog + ' ' + s.active}>
+                  Vovan
+              </div>
+              <div className={s.dialog}>
+                  Sanches
+              </div>
+              <div className={s.dialog}>
+                  Лена
+              </div>
+              <div className={s.dialog}>
+                  Миша
+              </div>
+              <div className={s.dialog}>
+                  Лёша
+              </div>
+          </div>
+          <div className={s.messages}>
+              <div className={s.message}>Hi</div>
+              <div className={s.message}>How are you?</div>
+              <div className={s.message}>Yo</div>
+          </div>
       </div>
   )
 }
