06fbedbf875bbf85d5c04f7e784f7013f3695269
Vladimir Blinov <vladimir.a.blinov@gmail.com>
2023-03-27 13:52:56 +0300

    подчистил презентационную компоненту Users.jsx

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,27 +1,70 @@
-import React, {useId} from "react";
-import Users from "./Users";
+import React from "react";
 import {connect} from "react-redux";
-import {followAC, setUsersAC, unfollowAC} from "../../redux/usersReducer";
+import {followAC, setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC} from "../../redux/usersReducer";
+import axios from "axios";
+import Users from "./Users";
+
+class UsersContainer extends React.Component {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            });
+    }
+
+
+    render() {
+        return <Users
+            totalUsersCount={this.props.totalUsersCount}
+            pageSize={this.props.pageSize}
+            currentPage={this.props.currentPage}
+            onPageChanged={this.onPageChanged}
+            users={this.props.users}
+            follow={this.props.follow}
+            unfollow={this.props.unfollow}
+
+        />
+    }
+}
 
 const mapStateToProps = (state) => {
   return {
-      users: state.usersPage.users
+      users: state.usersPage.users,
+      pageSize: state.usersPage.pageSize,
+      totalUsersCount: state.usersPage.totalUsersCount,
+      currentPage: state.usersPage.currentPage,
   }
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {
-      follow: (useId)=> {
-          dispatch(followAC(useId))
+      follow: (userId)=> {
+          dispatch(followAC(userId))
       },
-      unfollow: (useId)=> {
-          dispatch(unfollowAC(useId))
+      unfollow: (userId)=> {
+          dispatch(unfollowAC(userId))
       },
       setUsers: (users)=>{
           dispatch(setUsersAC(users))
+      },
+      setCurrentPage: (pageNumber) => {
+          dispatch(setCurrentPageAC(pageNumber))
+      },
+      setTotalUsersCount: (totalCount) => {
+          dispatch(setTotalUsersCountAC(totalCount))
       }
   }
 }
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(Users);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
